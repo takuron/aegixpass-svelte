72280e41886117008fa9bc4f4f4f95bb4d6936a7
Takuron <25877469+takuron@users.noreply.github.com>
2026-04-28 16:26:29 +0800

    rm

--- AegixPass/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// 声明 `core` 模块，它包含了所有的核心实现。
-pub mod core;
-
-// 将 `core` 模块中的关键公共项重新导出到库的顶层命名空间。
-// 这样外部使用者就可以通过 `aegixpass::aegixpass_generator` 的方式直接调用，
-// 而不是 `aegixpass::core::aegixpass_generator`，让 API 更简洁。
-pub use crate::core::{aegixpass_generator, AegixPassError, Preset};
-
